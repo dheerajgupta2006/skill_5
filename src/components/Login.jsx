e42b3c2a8eb001f197550f22e123700abd88cc42
dheerajgupta2006 <162675205+dheerajgupta2006@users.noreply.github.com>
2025-07-31 12:23:16 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
     <div className="center-layout">
       <div className="auth-container">
         <h2>Login</h2>
-        <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="password" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
         <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
         <button onClick={handleLogin}>Login</button>
         <p>Don't have an account? <Link to="/signup">Sign up</Link></p>
